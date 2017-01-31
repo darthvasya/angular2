21f4dad057e04a4aafb8eccb69804d4ed40b9d73
Vasya Makarchuk <vasyamakarchuk@outlook.com>
2017-01-31 18:15:27 +0300

    components

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    templateUrl: 'app/components/app.html',
+    styleUrls: ['app/css/style.css']
+})
+export class AppComponent {
+    name= '';
+}
